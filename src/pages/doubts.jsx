6debf5bbfbe941a68ad7a3f468c0dc1159b85bc5
abhaumika <abhaumika@gmail.com>
2022-10-18 22:22:47 +0530

    new update

--- src/pages/doubts.jsx
+++ src/pages/doubts.jsx
@@ -1,7 +1,11 @@
+import RichTextEditor from "@mantine/rte";
+
 const Doubts = () => {
   return (
     <div>
       <p className="text-3xl">Doubts</p>
+      <RichTextEditor sticky={false} />
+
       {/* option to filter based on dept (from course_id) */}
     </div>
   );
